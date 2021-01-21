c2c351d468d815279f2f00192d395343841bdac7
Quazi Marufur Rahman <quazi.rahman@qut.edu.au>
2021-01-21 14:32:47 +1000

    init commit

---
+++ examples/my_test.py
@@ -0,0 +1,27 @@
+import glob
+import os
+import sys
+import time
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+from carla import VehicleLightState as vls
+
+import argparse
+import logging
+from numpy import random
+
+client = carla.Client('localhost', 2000)
+print (client)
+client.set_timeout(10.0) # seconds
+
+world = client.get_world()
+print(client.get_available_maps())
